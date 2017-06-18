acaea4e85e8484e1476d111d84c17b4cd85843bc
Jenu Patel <jenu.16@icloud.com>
2017-06-18 00:09:37 -0500

    Using the executemany() method

---
+++ 03_sql.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# executemany() method
+
+import sqlite3
+
+with sqlite3.connect("new.db") as connection:
+    c = connection.cursor()
+
+    # insert mutliple records using a tuple
+    cities = [
+        ('Boston', 'MA', 600000),
+        ('Chicago', 'IL', 2700000),
+        ('Houston', 'TX', 2100000),
+        ('Phoenix', 'AZ', 1500000)
+    ]
+
+    # insert data into table
+    c.executemany('INSERT INTO population VALUES(?, ?, ?)', cities)
